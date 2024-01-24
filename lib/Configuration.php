a0a8a35f18ec33a1d32cac1a455510cb5e8e8d4f
chapellier <logan.chapellier3@etu.univ-lorraine.fr>
2024-01-24 14:35:32 +0100

    day

--- lib/Configuration.php
+++ lib/Configuration.php
@@ -60,11 +60,9 @@ class Configuration
             'compression'              => 'zlib',
         ),
         'expire' => array(
-<<<<<<< HEAD
             'default' => '1day',
-=======
-            'default' => '1month',
->>>>>>> change-default-expiration
+
+	 change-default-expiration
         ),
         'expire_options' => array(
             '5min'   => 300,
